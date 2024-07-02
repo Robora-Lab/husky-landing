732725a5ca5dc872558df715f2fddddae91728a7
WilStew <160658202+WilStew@users.noreply.github.com>
2024-07-02 11:06:36 -0400

    Delete devel directory

--- devel/lib/python3/dist-packages/open_nmpc_controller/msg/__init__.py
+++
@@ -1,3 +0,0 @@
-from ._OptimizationParameters import *
-from ._OptimizationResult import *
-from ._Status import *
--- devel/share/open_nmpc_controller/cmake/open_nmpc_controller-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(open_nmpc_controller_MESSAGE_FILES "/home/will/mpc_ws/src/open_nmpc_controller/msg/OptimizationResult.msg;/home/will/mpc_ws/src/open_nmpc_controller/msg/OptimizationParameters.msg;/home/will/mpc_ws/src/open_nmpc_controller/msg/Status.msg")
-set(open_nmpc_controller_SERVICE_FILES "")
